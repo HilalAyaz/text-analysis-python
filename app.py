58e3410d042ae6ca38b7c02bc11c9bf996dc737b
Hilal Ayaz <134634880+HilalAyaz@users.noreply.github.com>
2024-03-10 14:26:33 +0500

    .

--- app.py
+++ app.py
@@ -38,3 +38,5 @@ def index():
         return render_template('index.html', results=analysis_results)
     return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
